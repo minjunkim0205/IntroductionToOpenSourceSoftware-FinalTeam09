5c3172d3de0a77e64e22040c2b5151b738429144
minjunkim0205 <minjunkim0205@gmail.com>
2025-11-26 12:30:43 +0900

    Update module/gpt.py - api_check() function

--- module/gpt.py
+++ module/gpt.py
@@ -3,17 +3,35 @@
 # ---------------------------------------------------
 # Import module
 # ---------------------------------------------------
-import openai
+from openai import OpenAI
 
 # ---------------------------------------------------
 # Function
 # ---------------------------------------------------
-def api_check():
+def api_check(_key:str) -> bool:
+    client = OpenAI(api_key=_key)
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "user", "content": "hello"}
+            ]
+        )
+        # print(response.choices[0].message["content"])
+        return True
+    except Exception as e:
+        # print(e)
+        return False
+
+def api_repository_structure(_key:str, _file_tree:dict) -> str:
     pass
 
 # ---------------------------------------------------
 # Test
 # ---------------------------------------------------
 if __name__=="__main__":
-    key = ""
-    print(key, end="")
+    key = str(input("Your GPT API key >> "))
+    if api_check(key):
+        print("Success", end="")
+    else:
+        print("Error", end="")
