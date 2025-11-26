aec9458e1ed8a1a69b1289d1c3ccae21c377d2de
minjunkim0205 <minjunkim0205@gmail.com>
2025-11-27 08:33:17 +0900

    Update module/gemini.py

--- module/gemini.py
+++ module/gemini.py
@@ -4,6 +4,7 @@
 # Import module
 # ---------------------------------------------------
 from google import genai
+from google.genai import types
 
 # ---------------------------------------------------
 # Function
@@ -13,9 +14,14 @@ def api_check(_key: str) -> bool:
     try:
         response = client.models.generate_content(
             model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                # 반드시 영어로 대답 해야합니다.
+                system_instruction="You must answer in English."
+            ),
+            # 핑
             contents="ping"
         )
-        # print(response.text)
+        print(response.text)
         return True
     except Exception as e:
         # print(e)
